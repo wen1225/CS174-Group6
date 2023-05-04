c2241038d81ae5d8e013dbee16a5d6a29e237231
phuihumu <priscilla.hui@sjsu.edu>
2023-05-03 18:38:41 -0700

    Created base Login and SignUp pages.

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,85 @@
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import { createTheme, ThemeProvider} from '@mui/material/styles';
+
+const theme = createTheme();
+
+export default function Login() {
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const data = new FormData(event.currentTarget)
+        console.log({
+            username: data.get('username'),
+            password: data.get('password'),
+        })
+    }
+    
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 15,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        padding: 2,
+                        boxShadow: 2
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Sign In
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Typography
+                            textAlign="left"
+                            fontWeight="bold"
+                        >
+                            Username
+                        </Typography>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Enter Username"
+                            name="username"
+                        />
+                        <Typography
+                            textAlign="left"
+                            fontWeight="bold"
+                        >
+                            Password
+                        </Typography>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="password"
+                            label="Enter Password"
+                            name="password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 , boxShadow: "none", textTransform: "none"}}
+                        >
+                            Sign In
+                        </Button>
+                        <Link href="/signup" variant="body2">
+                            Dont have an account? Sign Up
+                        </Link>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    )
+}
