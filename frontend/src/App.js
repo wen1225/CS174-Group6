3dfe83c7842b8045d8ef828c87f6dc75be19b036
phuihumu <priscilla.hui@sjsu.edu>
2023-05-08 16:13:22 -0700

    Created ui for Members list, CreateMemberForm, and UpdateMemberForm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from "react"
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import Members from './pages/Members'
+import UpdateMemberForm from './components/UpdateMemberForm'
+import CreateMemberForm from './components/CreateMemberForm'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/member/:id" element={<UpdateMemberForm />} />
+          <Route path="/member/" element={<Members />} />
+          <Route path="/member/create-member" element={<CreateMemberForm />} />
         </Routes>
       </Router>
     </div>
